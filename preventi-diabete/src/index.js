5291b8d846960acce1cbb4332ab0783010b16ee2
Hamza Bourijal <hamzavshum@gmail.com>
2024-09-13 10:53:13 +0100

    3rd

--- preventi-diabete/src/index.js
+++ preventi-diabete/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.css';  // Global CSS file for additional styles
+import App from './App';  // Main component
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')  // Renders the app in the div with id 'root'
 );
